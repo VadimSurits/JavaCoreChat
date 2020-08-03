d4d031cde326332cc2a3a3ffec0e8ce27775920e
VadimSurits <Sourits-moscow@mail.ru>
2020-08-03 15:34:48 +1000

    Logger added in Server.java and Start.java

--- server/src/main/java/server/Start.java
+++ server/src/main/java/server/Start.java
@@ -1,7 +1,18 @@
 package server;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 public class Start {
+    private static final Logger logger = Logger.getLogger("");
+
     public static void main(String[] args) {
+        try {
+            logger.addHandler(new FileHandler("chat.log", true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         new Server();
     }
 }
